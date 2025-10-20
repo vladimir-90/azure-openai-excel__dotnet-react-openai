0db4bc9d5c77862ba0e73f1480e7b988532828c3
Vlad <vladimirtimoshenko90@gmail.com>
2025-10-20 18:26:07 +0300

    web back-end - GET available-ai-models endpoint

---
+++ ExcelAnalysisAI.Web.Server/Controllers/GenerationSettingsController.cs
@@ -0,0 +1,16 @@
+﻿using ExcelAnalysisAI.AzureOpenAI.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ExcelAnalysisAI.Web.Server.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class GenerationSettingsController : ControllerBase
+{
+    [HttpGet("available-ai-models")]
+    public IActionResult GetAvailableModels()
+    {
+        var options = (OpenAIModelType[])Enum.GetValues(typeof(OpenAIModelType));
+        return Ok(options!.Select(x => x.ToString()).ToArray());
+    }
+}
