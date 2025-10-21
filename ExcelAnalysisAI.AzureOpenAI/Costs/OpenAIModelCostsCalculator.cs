9fdf2e97ee739f13ca4a5e4f5d363e73475d1be7
Vlad <vladimirtimoshenko90@gmail.com>
2025-10-21 21:10:41 +0300

    code structuring - OpenAI pricings extracted from OpenAIModelCostsCalculator into separate classes

--- ExcelAnalysisAI.AzureOpenAI/Costs/OpenAIModelCostsCalculator.cs
+++ ExcelAnalysisAI.AzureOpenAI/Costs/OpenAIModelCostsCalculator.cs
@@ -1,54 +1,15 @@
 ﻿using ExcelAnalysisAI.AzureOpenAI.Models;
+using ExcelAnalysisAI.AzureOpenAI.Pricings;
 using Microsoft.SemanticKernel;
 using OpenAI.Chat;
 
 namespace ExcelAnalysisAI.AzureOpenAI.Costs;
 
-//      Pricing is taken from here on 2025-10-21:
-// https://azure.microsoft.com/en-us/pricing/details/cognitive-services/openai-service/
-
 public static class OpenAIModelCostsCalculator
 {
-    // Azure OpenAI pricings (per 1M Tokens, 2025-10-14)
-    private static readonly Dictionary<OpenAIModelType, OpenAIModelPricing> _pricings = new()
-    {
-        {
-            OpenAIModelType.GPT_41_nano,
-            new OpenAIModelPricing{ Input = 0.10m, CachedInput = 0.03m, Output = 0.40m }
-        },
-        {
-            OpenAIModelType.GPT_41_mini,
-            new OpenAIModelPricing{ Input = 0.40m, CachedInput = 0.10m, Output =  1.60m}
-        },
-        {
-            OpenAIModelType.GPT_41,
-            new OpenAIModelPricing{ Input = 0.40m, CachedInput = 0.10m, Output =  8}
-        },
-        {
-            OpenAIModelType.GPT_5_nano,
-            new OpenAIModelPricing{ Input = 0.05m, CachedInput = 0.01m, Output = 0.4m }
-        },
-        {
-            OpenAIModelType.GPT_5_mini,
-            new OpenAIModelPricing{ Input = 0.40m, CachedInput = 0.10m, Output = 2 }
-        },
-        {
-            OpenAIModelType.GPT_5_chat,
-            new OpenAIModelPricing{ Input = 0.40m, CachedInput = 0.10m, Output = 10 }
-        },
-        {
-            OpenAIModelType.GPT_o3_mini,
-            new OpenAIModelPricing{ Input = 0.40m, CachedInput = 0.10m, Output = 1.60m}
-        },
-        {
-            OpenAIModelType.GPT_o4_mini,
-            new OpenAIModelPricing{ Input = 0.40m, CachedInput = 0.10m, Output = 1.60m}
-        },
-    };
-
     public static decimal CalculateCost(FunctionResult requestResult, OpenAIModelType modelType)
     {
-        var modelPricing = _pricings[modelType];
+        var modelPricing = OpenAIModelPricing.ForModel(modelType);
 
         if (requestResult.Metadata!.TryGetValue("Usage", out var usageObj) && usageObj is ChatTokenUsage usage)
         {
@@ -62,7 +23,7 @@ public static class OpenAIModelCostsCalculator
 
     public static QueryDetailedCost? CalculateDetailedCost(FunctionResult requestResult, OpenAIModelType modelType)
     {
-        var modelPricing = _pricings[modelType];
+        var modelPricing = OpenAIModelPricing.ForModel(modelType);
 
         if (requestResult.Metadata!.TryGetValue("Usage", out var usageObj) && usageObj is ChatTokenUsage usage)
         {
@@ -80,10 +41,4 @@ public static class OpenAIModelCostsCalculator
         return null;
     }
 
-    private class OpenAIModelPricing
-    {
-        public required decimal Input { get; init; }
-        public required decimal CachedInput { get; init; }
-        public required decimal Output { get; init; }
-    }
 }
---
+++ ExcelAnalysisAI.AzureOpenAI/Pricing/OpenAIModelPricing.cs
@@ -0,0 +1,50 @@
+﻿using ExcelAnalysisAI.AzureOpenAI.Models;
+
+namespace ExcelAnalysisAI.AzureOpenAI.Pricings;
+
+public class OpenAIModelPricing
+{
+    public required decimal Input { get; init; }
+    public required decimal CachedInput { get; init; }
+    public required decimal Output { get; init; }
+
+    // Azure OpenAI pricings (per 1M Tokens, 2025-10-21)
+    // https://azure.microsoft.com/en-us/pricing/details/cognitive-services/openai-service/
+    private static readonly IReadOnlyDictionary<OpenAIModelType, OpenAIModelPricing> _pricings = new Dictionary<OpenAIModelType, OpenAIModelPricing>
+    {
+        {
+            OpenAIModelType.GPT_41_nano,
+            new OpenAIModelPricing{ Input = 0.10m, CachedInput = 0.03m, Output = 0.40m }
+        },
+        {
+            OpenAIModelType.GPT_41_mini,
+            new OpenAIModelPricing{ Input = 0.40m, CachedInput = 0.10m, Output =  1.60m}
+        },
+        {
+            OpenAIModelType.GPT_41,
+            new OpenAIModelPricing{ Input = 0.40m, CachedInput = 0.10m, Output =  8}
+        },
+        {
+            OpenAIModelType.GPT_5_nano,
+            new OpenAIModelPricing{ Input = 0.05m, CachedInput = 0.01m, Output = 0.4m }
+        },
+        {
+            OpenAIModelType.GPT_5_mini,
+            new OpenAIModelPricing{ Input = 0.40m, CachedInput = 0.10m, Output = 2 }
+        },
+        {
+            OpenAIModelType.GPT_5_chat,
+            new OpenAIModelPricing{ Input = 0.40m, CachedInput = 0.10m, Output = 10 }
+        },
+        {
+            OpenAIModelType.GPT_o3_mini,
+            new OpenAIModelPricing{ Input = 0.40m, CachedInput = 0.10m, Output = 1.60m}
+        },
+        {
+            OpenAIModelType.GPT_o4_mini,
+            new OpenAIModelPricing{ Input = 0.40m, CachedInput = 0.10m, Output = 1.60m}
+        },
+    };
+
+    public static OpenAIModelPricing ForModel(OpenAIModelType modelType) => _pricings[modelType];
+}
