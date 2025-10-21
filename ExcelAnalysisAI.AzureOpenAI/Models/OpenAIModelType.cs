98eccd38ff1fe1409c061b56fe7acb1c9553281e
Vlad <vladimirtimoshenko90@gmail.com>
2025-10-22 01:22:50 +0300

    web back-end - GenerationSettings endpoints improvement to return "more" data

--- ExcelAnalysisAI.AzureOpenAI/Models/OpenAIModelType.cs
+++ ExcelAnalysisAI.AzureOpenAI/Models/OpenAIModelType.cs
@@ -1,13 +1,30 @@
-﻿namespace ExcelAnalysisAI.AzureOpenAI.Models;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ExcelAnalysisAI.AzureOpenAI.Models;
 
 public enum OpenAIModelType
 {
+    [Display(Name = "gpt-4.1-nano")]
     GPT_41_nano,
+
+    [Display(Name = "gpt-4.1-mini")]
     GPT_41_mini,
+
+    [Display(Name = "gpt-4.1")]
     GPT_41,
+
+    [Display(Name = "gpt-5-nano")]
     GPT_5_nano,
+
+    [Display(Name = "gpt-5-mini")]
     GPT_5_mini,
+
+    [Display(Name = "gpt-5-chat")]
     GPT_5_chat,
+
+    [Display(Name = "o3-mini")]
     GPT_o3_mini,
+
+    [Display(Name = "o4-mini")]
     GPT_o4_mini
 }
--- ExcelAnalysisAI.Web.Server/Controllers/GenerationSettingsController.cs
+++ ExcelAnalysisAI.Web.Server/Controllers/GenerationSettingsController.cs
@@ -1,5 +1,9 @@
 ﻿using ExcelAnalysisAI.AzureOpenAI.Models;
+using ExcelAnalysisAI.AzureOpenAI.Pricings;
+using ExcelAnalysisAI.Core.Utility;
 using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel.DataAnnotations;
+using System.Reflection;
 
 namespace ExcelAnalysisAI.Web.Server.Controllers;
 
@@ -11,7 +15,14 @@ public class GenerationSettingsController(IWebHostEnvironment _env) : Controller
     public IActionResult GetAvailableModels()
     {
         var modelTypes = (OpenAIModelType[])Enum.GetValues(typeof(OpenAIModelType));
-        var options = modelTypes!.Select(x => x.ToString()).ToArray();
+        var options = modelTypes!
+            .Select(x => new AIModelDto
+            {
+                ModelType = x.ToString(),
+                Label = x.GetType().GetField(x.ToString())!.GetCustomAttribute<DisplayAttribute>()!.Name!,
+                Pricing = OpenAIModelPricing.ForModel(x)
+            })
+            .ToArray();
         return Ok(options);
     }
 
@@ -23,4 +34,35 @@ public class GenerationSettingsController(IWebHostEnvironment _env) : Controller
         var options = dataDirs.Select(x => new DirectoryInfo(x).Name).ToArray();
         return Ok(options);
     }
+
+    [HttpGet("test-data-sets/{datasetName}")]
+    public async Task<IActionResult> GetTestDataset([FromRoute] string datasetName)
+    {
+        var dirPath = Path.Combine(_env.ContentRootPath, "Data", datasetName);
+        var schema = await System.IO.File.ReadAllTextAsync(Path.Combine(dirPath, "schema.txt"));
+        var data = ExcelUtility.ReadWorksheet(Path.Combine(dirPath, "data.xlsx"), "Employees");
+
+        return Ok(new DataSetInfoDto
+        {
+            Name = datasetName,
+            Schema = schema,
+            DataSlice = data.Take(5).ToList(),
+            TotalEntityCount = data.Count
+        });
+    }
+}
+
+public class AIModelDto
+{
+    public required string ModelType { get; set; }
+    public required string Label { get; set; }
+    public required OpenAIModelPricing Pricing { get; set; }
+}
+
+public class DataSetInfoDto
+{
+    public required string Name { get; set; }
+    public required string Schema { get; set; }
+    public required List<List<object>> DataSlice { get; set; }
+    public required int TotalEntityCount { get; set; }
 }
