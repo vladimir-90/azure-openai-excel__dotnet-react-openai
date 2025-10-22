922570bbb5422ace66dd4b58c229d247d526508b
Vlad <vladimirtimoshenko90@gmail.com>
2025-10-22 06:23:06 +0300

    web back-end - JsonStringEnumConverter added to capture enum values in DTO-s when accepting incoming requests

--- ExcelAnalysisAI.Web.Server/Program.cs
+++ ExcelAnalysisAI.Web.Server/Program.cs
@@ -1,10 +1,15 @@
 using ExcelAnalysisAI.Web.Server.Infrastructure;
+using System.Text.Json.Serialization;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-builder.Services.AddControllers();
+builder.Services.AddControllers()
+    .AddJsonOptions(options =>
+    {
+        options.JsonSerializerOptions.Converters.Add(new JsonStringEnumConverter());
+    });
 
 builder.Configuration.AddJsonFile("appsettings.azure-openai.json");
 builder.Services.AddSingleton(builder.Configuration.GetSection("AzureOpenAI").Get<AzureOpenAIConfig>()!);
